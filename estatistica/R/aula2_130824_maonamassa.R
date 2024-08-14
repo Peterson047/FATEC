69e10548abfff8fec1d10c7d20735de7d9f0a42b
Peterson Alves <peterrson047@gmail.com>
2024-08-13 22:18:00 -0300

    Add files via upload

---
+++ estatistica/R/aula2_130824_maonamassa.R
@@ -0,0 +1,16 @@
+mediax<-2+2+5+7+8+9+20
+mediax<-mediax/7
+
+mediay<-3+3+4+5+5+7+8+9+9+14
+mediay<-mediay/10
+
+mediaz<-2.34+4.56+5.35+6.45
+mediaz<-mediaz/4
+
+
+teste<-function(x){
+  y<-x^2
+  return(y)
+}
+x<-3
+teste(x)

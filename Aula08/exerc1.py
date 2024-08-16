55e87403ca5c87116f3ef3d4487269d62206ed4f
unknown <peterrson047@gmail>
2024-08-15 21:03:49 -0300

    update

--- Aula08/exerc1.py
+++
@@ -1,6 +0,0 @@
-num1 = input ("Digite um número: ")
-num2 = input ("Digite outro número: ")
-if num1 > num2:
-    print ("O primeiro número é maior")
-else:
-    print ("O segundo número é maior")    
